93170906204e2d910c1f75f984ecef2979a16aab
eKemble <epkqmb@mail.missouri.edu>
2014-11-25 21:16:51 -0600

    Update warhammer.sql

--- warhammer.sql
+++ warhammer.sql
@@ -41,6 +41,19 @@ INSERT INTO unit_list(1, 'Tactical Squad', default, 'Troops', '5 Space Marines',
 INSERT INTO unit_list(1, 'Scout Squad', default, 'Troops', '5 Scouts', 75, 13, 5, 3, 3, 4, 4, 1, 4, 1, 8, 4);
 INSERT INTO unit_list(1, 'Terminator Squad', default, 'Elites', '5 Terminators', 200, 40, 5, 4, 4, 4, 4, 1, 4, 2, 9, 2);
 INSERT INTO unit_list(1, 'Techmarine', default, 'Elites', '1 Techmarine', 50, null, null, 4, 4, 4, 4, 1, 4, 1, 8, 2);
+INSERT INTO unit_list(1, 'Assault Squad', default, 'Fast Attack', '5 Space Marines', 100, 18, 5, 4, 4, 4, 4, 1, 4, 1, 8, 3);
+
+
+INSERT INTO unit_list(2, 'Warboss', default, 'HQ', '1 Warboss', 60, null, null, 5, 2, 5, 5, 3, 4, 4, 9, 6);
+INSERT INTO unit_list(2, 'Big Mek', default, 'HQ', '1 Big Mek', 35, null, null, 4, 2, 4, 4, 2, 3, 3, 8, 6);
+INSERT INTO unit_list(2, 'Weirdboy', default, 'HQ', '1 Weirdboy', 55, null null, 4, 2, 4, 4, 2, 3, 3, 7, 6);
+INSERT INTO unit_list(2, 'Ork Boyz', default, 'Troops', '10 Ork Boyz', 60, 6, 20, 4, 2, 3, 4, 1, 2, 2, 7, 6);
+INSERT INTO unit_list(2, 'Gretchin', default, 'Troops', '10 Gretchin', 30, 3, 20, 2, 3, 2, 2, 1, 2, 1, 5, null);
+INSERT INTO unit_list(2, 'Nobz', default, 'Elites', '3 Nobz', 60, 20, 7, 4, 2, 4, 4, 2, 3, 3, 7, 6);
+INSERT INTO unit_list(2, 'Burna Boyz', default, 'Elites', '5 Burna Boyz', 75, 15, 10, 4, 2, 3, 4, 1, 2, 2, 7, 6);
+INSERT INTO unit_list(2, 'Tankbustas', default, 'Elites', '5 Tankbusta Boyz', 75, 15, 10, 4, 2, 3, 4, 1, 2, 2, 7, 6);
+INSERT INTO unit_list(2, 'Warbikers', default, 'Fast Attack', '3 Warbikes', 75, 25, 9, 4, 2, 3, '4(5)', 1, 2, 2, 7, 4);
+INSERT INTO unit_list(2, 'Deffkoptas', default, 'Fast Attack', '1 Deffkopta', 35, 35, 4, 4, 2, 3, '4(5)', 2, 2, 2, 7, 4);
 
 DROP TABLE IF EXISTS vehicle_list;
 CREATE TABLE vehicle_list(
@@ -64,13 +77,19 @@ CREATE TABLE vehicle_list(
    attacks (int)
 );
 
-INSERT INTO vehicle_list( 1,'Rhino', default, 'Dedicated Transport', 35, null, 4, null, 11, 11, 10, null, null);
-INSERT INTO vehicle_list( 1,'Drop Pod', default, 'Dedicated Transport', 35, null, 4, null, 12, 12, 12, null, null);
-INSERT INTO vehicle_list( 1,'Dreadnought', default, 'Elite', 105, 4, 4, 6, 12, 12, 10, 4, 2);
-INSERT INTO vehicle_list( 1,'Land Raider', default, 'Heavy Support', 250, null, 4, null, 14, 14, 14, null, null);
-INSERT INTO vehicle_list( 1,'Predator', default, 'Heavy Support', 60, null, 4, null, 13, 11, 10, null, null);
-INSERT INTO vehicle_list( 1,'Whirlwind', default, 'Heavy Support', 85, null, 4, null, 11, 11, 10, null, null);
-INSERT INTO vehicle_list( 1,'Vindicator', default, 'Heavy Support' 115, null, 4, null, 13, 11, 10, null, null);
+INSERT INTO vehicle_list( 1, 'Rhino', default, 'Dedicated Transport', 35, null, 4, null, 11, 11, 10, null, null);
+INSERT INTO vehicle_list( 1, 'Drop Pod', default, 'Dedicated Transport', 35, null, 4, null, 12, 12, 12, null, null);
+INSERT INTO vehicle_list( 1, 'Dreadnought', default, 'Elite', 105, 4, 4, 6, 12, 12, 10, 4, 2);
+INSERT INTO vehicle_list( 1, 'Land Raider', default, 'Heavy Support', 250, null, 4, null, 14, 14, 14, null, null);
+INSERT INTO vehicle_list( 1, 'Predator', default, 'Heavy Support', 60, null, 4, null, 13, 11, 10, null, null);
+INSERT INTO vehicle_list( 1, 'Whirlwind', default, 'Heavy Support', 85, null, 4, null, 11, 11, 10, null, null);
+INSERT INTO vehicle_list( 1, 'Vindicator', default, 'Heavy Support' 115, null, 4, null, 13, 11, 10, null, null);
+
+INSERT INTO vehicle_list( 2, 'Trukk', default, 'Dedicated Transport', 35, null, 2, null, 10, 10, 10, null, null);
+INSERT INTO vehicle_list( 2, 'Battlewagon', default, 'Heavy Support', 90, null, 2, null, 14, 12, 10, null, null);
+INSERT INTO vehicle_list( 2, 'Deff Dread', default, 'Heavy Support', 75, 4, 2, 5, 12, 12, 10, 2, 3);
+INSERT INTO vehicle_list( 2, 'Killa Kans', default, 'Heavy Support', 35, 2, 3, 5, 11, 11, 10, 2, 2);
+INSERT INTO vehicle_list( 2, 'Looted Wagon', default, 'Heavy Support', 35, null, 2, null, 11, 11, 10, null, null);
 
 
    -- Separate table for special rules
@@ -120,6 +139,7 @@ INSERT INTO weapons(1, default, 'Multi-Melta', 24, 8, 1, 'Heavy1, Melta', 0);
 INSERT INTO weapons(1, default, 'Plasma Cannon', 36, 7, 2, 'Heavy1, Blast, Gets Hot!', 0);
 INSERT INTO weapons(1, default, 'Plasma Gun', 24, 7, 2, 'Rapid Fire, Gets Hot!', 0);
 INSERT INTO weapons(1, default, 'Plasma Pistol', 12, 7, 2, 'Pistol, Gets Hot!', 0);
+INSERT INTO weapons(1, default, 'Power Fist', '-', 'X2', 2, 'Melee, Specialist Weapon, Unwieldy', 0);
 INSERT INTO weapons(1, default, 'Shotgun', 12, 4, '-', 'Assault2', 0);
 INSERT INTO weapons(1, default, 'Sniper Rifle', 36, 'X', 6, 'Heavy1, Sniper', 0);
 INSERT INTO weapons(1, default, 'Storm Bolter', 24, 4, 5, 'Assault2', 0);
@@ -128,6 +148,25 @@ INSERT INTO weapons(1, default, 'Typhoon Missile Launcher (Frag / Krak)', '48 / 
 INSERT INTO weapons(1, default, 'Vengence round', 18, 4, 3, 'Rapid Fire, Gets Hot!', 0);
 
 INSERT INTO weapons(2, default, 'Big Shoota', 36, 5, 5, 'Assault3', 0);
+INSERT INTO weapons(2, default, 'Boomgun', 36, 8, 3, 'Ordinance1, Large Blast', 0);
+INSERT INTO weapons(2, default, 'Burnas', 'Template', 4, 5, 'Assault1', 0);
+INSERT INTO weapons(2, default, 'Da Rippa', 24, 7, 2, 'Assault3, Gets Hot!', 0);
+INSERT INTO weapons(2, default, 'Dakkagun', 18, 5, 5, 'Assault3', 0);
+INSERT INTO weapons(2, default, 'Dakkakannon', 24, 8, 4, 'Assault4', 0);
+INSERT INTO weapons(2, default, 'Deffgun', 48, 7, 4, 'Heavy D3', 0);
+INSERT INTO weapons(2, default, 'Grot Blasta', 12, 3, '-', 'Assault1', 0);
+INSERT INTO weapons(2, default, 'Grotzooka', 18, 6, 5, 'Heavy2, Blast', 0);
+INSERT INTO weapons(2, default, 'Kannon (Frag / Shell)', '36 / 36', '4 / 8', '5 / 3', 'Heavy1, Blast / Heavy1', 0);
+INSERT INTO weapons(2, default, 'Killkannon', 24, 7, 3, 'Ordinance1, Large Blast', 0);
 INSERT INTO weapons(2, default, 'Kustom Mega Blasta', 24, 8, 2, 'Assault1, Gets Hot!', 0);
+INSERT INTO weapons(2, default, 'Launcha', 'G48', 5, 5, 'Heavy1, Blast', 0);
+INSERT INTO weapons(2, default, 'Power Klaw', '-', 'X2', 2, 'Melee, Specialist Weapon, Unwieldy', 0);
 INSERT INTO weapons(2, default, 'Rokkit Launcha', 24, 8, 3, 'Assault1', 0);
-INSERT INTO weapons(2, default, 'Scorcha', 'Template', 5, 4, 'Assault1', 0);
+INSERT INTO weapons(2, default, 'Skorcha', 'Template', 5, 4, 'Assault1', 0);
+INSERT INTO weapons(2, default, 'Shokk Attack Gun', '2D6', 2, 'Ordinance1, Large Blast', 0);
+INSERT INTO weapons(2, default, 'Shoota', 18, 4, 6, 'Assault2', 0);
+INSERT INTO weapons(2, default, 'Slugga', 12, 4, 6, 'Pistol', 0);
+INSERT INTO weapons(2, default, 'Snazzguns', 24, 5, '2D6', 'Assault1', 0);
+INSERT INTO weapons(2, default, 'Stikkbomb', 8, 3, null, 'Assualt1, Blast', 0); 
+INSERT INTO weapons(2, default, 'Zzap Gun', 36, '2D6', 2, 'Heavy1', 0);
+
