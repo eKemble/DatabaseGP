df5ae975c44a68b5bebee7bd5052fac0fe2de516
eKemble <epkqmb@mail.missouri.edu>
2014-11-29 20:19:24 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title> Warhammer 40k Army Companion</title>
+
+<div id="header">
+  <?php include 'header.php';?>
+</div>
+</head>
+
+<body>
+<h1>Burn the Heretic. Kill the Mutant. Purge the Unclean!</h1>
+</body>
+
+</html>
