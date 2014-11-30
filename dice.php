3eebbc4ab532795674e7a3d40c245043bf0741be
eKemble <epkqmb@mail.missouri.edu>
2014-11-29 20:20:35 -0600

    Create dice.php

---
+++ dice.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>Automated Dice Roller</title>
+
+<div id="header">
+  <?php include 'header.php';?>
+</div>
+</head>
+
+<body>
+<h1>Roll Dice here:</h1>
+<br>
+<p>(Sorry! Feature coming soon.) </p>
+</body>
+
+</html>
