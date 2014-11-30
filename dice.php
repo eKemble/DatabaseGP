67c5ab93c53dc78c53f21078e8a417bb0489fbb5
jacobnailor <jhn98f@mail.missouri.edu>
2014-11-29 21:00:01 -0600

    Finished Dice Roller
    Sorry it took so long, I completely forgot because apparently I have the memory of a goldfish.

--- dice.php
+++ dice.php
@@ -4,15 +4,37 @@
 <head>
 <title>Automated Dice Roller</title>
 
-<div id="header">
-  <?php include 'header.php';?>
-</div>
+
 </head>
 
 <body>
 <h1>Roll Dice here:</h1>
 <br>
-<p>(Sorry! Feature coming soon.) </p>
+<form action="../GroupProject/dice.php" method='post'>
+			  <label for="dice">Number of dice:</label>
+			  <input type="number" name="dice" id="dice" min="1" max="1000" >
+			  <br>
+			  <br>
+			  <input type="submit" name="submit" value="Roll!">
+		  </form> 
+<?php
+	if(isset($_POST['submit']))
+	{
+		$dice = $_POST['dice'];
+		
+		$i = 0;
+		
+		$diceCount[] = 0;
+		for($i=0;$i<$dice;$i++)
+		{
+			$diceCount[rand(1,6)]++;
+		}
+		
+		echo "<br>Dice Results:<br>";
+		echo "<br>1's: " . $diceCount[1] . "  <br>2's: " . $diceCount[2] . "  <br>3's: " . $diceCount[3] . "
+		  <br>4's: " . $diceCount[4] . "  <br>5's: " . $diceCount[5] . "  <br>6's: " . $diceCount[6];
+	}
+?>
 </body>
 
 </html>
