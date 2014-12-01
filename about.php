ab135871c0b293753e752dcf45eef05e3d35484c
bStrouse <btscnb@mail.missouri.edu>
2014-12-01 14:24:31 -0600

    Update about.php

--- about.php
+++ about.php
@@ -12,7 +12,23 @@
 <body>
 <h1>Our group project</h1>
 <br>
-<p>This is for our class!</p>
+<p>
+	The website that our group built is a Warhammer 40k army builder/companion. Warhammer is a tabletop miniature wargame that uses miniature
+scale-model characters to battle. Players must select units to form the most strategic army to beat the opponents. Then, a turn-based series of dice rolls and 
+measured movements take place and are used to fight. The game takes place well in the future and features many futuristic weapons and soldiers.
+	
+	The main purpose of our website application is to aid users in playing the game. It allows the user to select units from two separate armies and add them to 
+their own army. Each unit has their own specific abilities and their own separate costs. Once your allotted amount of cost points is reached, your army is finished.
+ Then, once there are two armies built, the user can then compare them using the battle calculator feature to simulate the outcome
+ of a battle. This can greatly help players trying to gain an advantage over the competition. There is also an automatic dice roller feature that aids in the playing of the game. 
+ This feature randomizes the rolling of up to 1000 dice and returns the number of each side represented. Having this feature greatly reduces the amount of time used to roll dice. 
+
+	We used php and html to code our website with a sql database. Our project leader, Eric Kemble, was responsible for the creation of the dataset along with the
+design and implementation of the site. The automatic dice-rolling feature was designed by Jacob Nailor. The battle calculator was created by Austin Kauble,
+along with overall design of the army builder. Brett Strouse was also responsible for design of the army builder as was Siting Cai.
+
+
+</p>
 <br>
 <?php   //Insert more text here about our project!   
 ?>
