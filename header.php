16c3bd8cf4b0faf2cb0b0a4416bc07c680898211
eKemble <epkqmb@mail.missouri.edu>
2014-12-01 09:51:46 -0600

    current header

--- header.php
+++ header.php
@@ -1,1 +1,2 @@
-<a href="../GroupProject/index.php">Home</a><p>  </p><a href="../GroupProject/dice.php">Auto Dice Roller</a>
+<link rel="stylesheet" type="text/css" href="css/warhammer_style.css"> 
+<a href="../GroupProject/index.php">Home</a> | <a href="../GroupProject/calculator.php">Battle Calculator</a> | <a href="../GroupProject/dice.php">Auto Dice Roller</a> | <a href="../GroupProject/about.php">About</a>  
