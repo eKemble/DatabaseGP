7a39e68b27871c766307e4b26f873fef246b74cd
austinkauble <awkppf@babbage.cs.missouri.edu>
2014-12-01 10:43:28 -0600

    Implemented unit adding dropdown fully

--- index.php
+++ index.php
@@ -19,24 +19,27 @@
 
 <?php
 	$conn = pg_connect("host=dbhost-pgsql.cs.missouri.edu port=5432 user= password=") or die('Could not connect: ' . pg_last_error());
-	//if(isset($_POST['submit'])){
-	//   if ($_POST['army'] == 1){
-	      	echo "<form action=\"../GroupProject/index.php\" method='post'>
-	           <select name=\"unit\">";
-	       	$result = pg_query("SELECT unit_id,unit_name,init_point_cost FROM warhammer.unit_list") or die('Query failed: ' . pg_last_error());
-			while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
-			{
-				echo "\t\t<option value=\"" . $line["unit_id"] . "\" selected=\"selected\">" . $line["unit_name"] ." - ". $line["init_point_cost"] ."</option>\n";
-	        }	
-	        echo "</select>";
-	  // loop the above option tag to have a drop down selecter for the available army units
-	 		echo "	<input type=\"submit\" name=\"submit\" value=\"Select\">";
-			echo "		</form>";
-	//   }
+	if(isset($_POST['submit'])){
+		add_unit_to_army($_POST['unit']);
+	}
+//   if ($_POST['army'] == 1){
+      	echo "<form action=\"index.php\" method='post'>
+           <select name=\"unit\">\n";
+       	$result = pg_query("SELECT unit_id,unit_name,init_point_cost FROM warhammer.unit_list WHERE army_id=1") or die('Query failed: ' . pg_last_error());
+		while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+		{
+			echo "\t\t<option value=\"" . $line["unit_id"] . "\" selected=\"selected\">" . $line["unit_name"] ." - ". $line["init_point_cost"] ."</option>\n";
+        }	
+        echo "</select>\n";
+  // loop the above option tag to have a drop down selecter for the available army units
+ 		echo "	<input type=\"submit\" name=\"submit\" value=\"Add Unit\">\n";
+		echo "		</form>\n";
 	//}
+	pg_free_result($result);
 
 	function add_unit_to_army($unit_id)
 	{
+		$conn = pg_connect("host=dbhost-pgsql.cs.missouri.edu port=5432 user= password=") or die('Could not connect: ' . pg_last_error());
 		$result = pg_prepare($conn, "point_query", "SELECT init_point_cost FROM warhammer.unit_list WHERE unit_id=$1");
 		$result = pg_execute($conn, "point_query", array($unit_id));
 		$result_a = pg_fetch_array($result,0,PGSQL_ASSOC);
