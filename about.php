996b6a05ed41c5d981ea7e1117521e284053f7a7
eKemble <epkqmb@mail.missouri.edu>
2014-12-01 09:51:02 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>About this project</title>
+
+<div id="header">
+  <?php include 'header.php';?>
+</div>
+</head>
+
+<body>
+<h1>Our group project</h1>
+<br>
+<p>This is for our class!</p>
+<br>
+<?php   //Insert more text here about our project!   
+?>
+</body>
+
+</html>
